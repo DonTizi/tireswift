8d78ce3234f85a54accf04f7250c458bdc507b20
RayaneMelDz <66443081+RayaneMelDz@users.noreply.github.com>
2024-01-21 10:16:20 -0500

    updated video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,51 +1,44 @@
-'use client'
-import React, { useState } from 'react';
-import Modal from 'react-modal';
-import { Button } from '@/components/Button';
-import { Container } from '@/components/Container';
+import Image from 'next/image'
 
-Modal.setAppElement('#root'); // Remplacez '#root' par l'ID de votre élément racine
+import { Button } from '@/components/Button'
+import { Container } from '@/components/Container'
+import logoLaravel from '@/images/logos/laravel.svg'
+import logoMirage from '@/images/logos/mirage.svg'
+import logoStatamic from '@/images/logos/statamic.svg'
+import logoStaticKit from '@/images/logos/statickit.svg'
+import logoTransistor from '@/images/logos/transistor.svg'
+import logoTuple from '@/images/logos/tuple.svg'
 
 export function Hero() {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  function openModal() {
-    setModalIsOpen(true);
-  }
-
-  function closeModal() {
-    setModalIsOpen(false);
-  }
-
   return (
     <Container className="pb-16 pt-20 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
         Tire Changing{' '}
         <span className="relative whitespace-nowrap text-blue-600">
+
           <span className="relative">made simple</span>
-        </span>
-        {' '}for you.
+        </span>{' '}
+        for you.
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-        Optimize your schedule and your income in real time.
+      Optimize your schedule and your income in real time.
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
         <Button href="/register">Try it for free.</Button>
-        <Button onClick={openModal} variant="outline">
-          Watch video
+        <Button
+          href="https://vimeo.com/904928121?share=copy#t=0 "
+          variant="outline"
+        >
+          <svg
+            aria-hidden="true"
+            className="h-3 w-3 flex-none fill-blue-600 group-active:fill-current"
+          >
+            <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
+          </svg>
+          <span className="ml-3">Watch video</span>
         </Button>
       </div>
-
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Video Modal" style={{ content: { top: '50%', left: '50%', right: 'auto', bottom: 'auto', marginRight: '-50%', transform: 'translate(-50%, -50%)' } }}>
-        <h2>Video Title</h2>
-        <button onClick={closeModal}>close</button>
-        <div>
-          <video width="100%" height="auto" controls>
-            <source src="path_to_your_video.mp4" type="video/mp4" />
-            Votre navigateur ne supporte pas la balise vidéo.
-          </video>
-        </div>
-      </Modal>
+    
     </Container>
-  );
+  )
 }
